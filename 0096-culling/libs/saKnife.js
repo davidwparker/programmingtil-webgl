ebcf06cfaeff8986d6c79673aa86354585d045f6
David Parker <davidwparker@gmail.com>
2017-05-14 17:58:14 -0600

    Screencast 0096

---
+++ 0096-culling/libs/saKnife.js
@@ -0,0 +1,20 @@
+(function(global){
+
+  /*
+  * saKnife "Swiss Army Knife"
+  * Utilities that are very generic for anything.
+  * www.programmingtil.com
+  * www.codenameparkerllc.com
+  */
+  var saKnife = {
+    VERSION : '0.0.1',
+    uuid: function() {
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+        return v.toString(16);
+      });
+    },
+  };
+  global.saKnife = saKnife;
+
+}(window || this));
